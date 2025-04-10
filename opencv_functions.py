8a7b1618d574188bb753802c182166bdcae0b5a8
twiggles <28828593+Twigums@users.noreply.github.com>
2025-04-09 18:34:24 -0700

    added a gitignore

--- opencv_functions.py
+++ opencv_functions.py
@@ -1,4 +1,6 @@
-import cv2
+# istg training a model wouldve been easier than doing this lol
+
+import apriltag, cv2
 import numpy as np
 
 
@@ -41,20 +43,83 @@ def get_camera_calibration(filepath):
 
     return camera_matrix, new_camera_matrix, distortion_coefficients
 
+def undistort_image(image, camera_matrix, distortion_coefficients):
+    height, width = image.shape[:2]
+    new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coefficients, (width, height), 1, (width, height))
+    x, y, roi_width, roi_height = roi
+    undistorted_image = cv2.undistort(image, camera_matrix, distortion_coefficients, None, new_camera_matrix)
+    undistorted_image = undistorted_image[y:y + roi_height, x:x + roi_width]
+
+    return undistorted_image
+
+def initialize_detector(flann_index_kdtree: int = 1, flann_trees: int = 5, flann_checks: int = 50):
+    sift = cv2.SIFT_create()
+    
+    index_params = dict(algorithm = flann_index_kdtree, trees = flann_trees)
+    search_params = dict(checks = flann_checks)
+     
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    return sift, flann
+    
+# https://docs.opencv.org/4.x/d1/de0/tutorial_py_feature_homography.html
+def get_detection(sift, flann, template, target, iterations: int = 10, flann_k: int = 2, min_match_count: int = 10, matching_ratio_score: float = 0.7, ransac_threshold: float = 5.0):
+    
+    # find the keypoints and descriptors with SIFT
+    kp_template, des_template = sift.detectAndCompute(template, None)
+    kp_target, des_target = sift.detectAndCompute(target, None)
+    
+    all_H = []
+
+    for i in range(iterations): 
+        matches = flann.knnMatch(des_template, des_target, k = flann_k)
+
+        # store all the good matches as per Lowe's ratio test.
+        good_matches = []
+        for m, n in matches:
+            if m.distance < matching_ratio_score * n.distance:
+                good_matches.append(m)
+
+        if len(good_matches) > min_match_count:
+            src_pts = np.float32([kp_template[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp_target[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+         
+            H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, ransac_threshold)
+            all_H.append(H)
+         
+        else:
+            print(f"Not enough matches are found in iter {i}: {len(good_matches)}/{min_match_count}")
+
+    h, w, _ = template.shape # not greyscale, so 3 channels
+    box_points = np.float32([
+        [0, 0],
+        [0, h - 1],
+        [w - 1, h - 1],
+        [w - 1, 0]
+        ]).reshape(-1, 1, 2)
+
+    avg_H = np.mean(np.array(all_H), axis = 0)
+    transformed_box = cv2.perspectiveTransform(box_points, avg_H)
+    
+    return transformed_box
+
 def img_to_np_gray(img: np.ndarray) -> np.ndarray:
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     np_gray_img = np.asarray(gray_img)
 
     return np_gray_img
 
-def draw_bbox(img: np.ndarray, tag: apriltag.Detection, color: tuple[int, int, int]) -> None:
+def draw_polygon(img: np.ndarray, points: np.ndarray, color: tuple[int, int, int] = (0, 255, 0), thickness: int = 3) -> None:
+    cv2.polylines(img, [np.int32(points)], True, color = color, thickness = thickness)
+
+def draw_apriltag_bbox(img: np.ndarray, tag: apriltag.Detection, color: tuple[int, int, int] = (0, 255, 0), thickness: int = 1) -> None:
     corner_points = tag.corners.astype(int)
     center = tag.center.astype(int)
 
-    cv2.polylines(img, [corner_points], isClosed = True, color = color, thickness = 1)
+    cv2.polylines(img, [corner_points], isClosed = True, color = color, thickness = thickness)
     cv2.circle(img, center, 1, color, -1)
 
-def draw_axis(img: np.ndarray, tag: apriltag.Detection, arrow_scale: float, text_offset: int, color: tuple[int, int, int]) -> None:
+def draw_axis(img: np.ndarray, tag: apriltag.Detection, color: tuple[int, int, int] = (0, 255, 0), arrow_scale: float = 0.1, text_offset: int = 10) -> None:
     corner_points = tag.corners.astype(int)
     center = tag.center.astype(int)
     x_vec = corner_points[1] - corner_points[0]
@@ -74,7 +139,7 @@ def draw_axis(img: np.ndarray, tag: apriltag.Detection, arrow_scale: float, text
     cv2.putText(img, "x", tuple(x_label_pos.astype(int)), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
     cv2.putText(img, "y", tuple(y_label_pos.astype(int)), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
 
-def draw_depth(img: np.ndarray, tag: apriltag.Detection, tag_size: float, camera_matrix: np.ndarray, distortion_coefficients: np.ndarray, color: tuple[int, int, int]) -> None:
+def draw_depth(img: np.ndarray, tag: apriltag.Detection, tag_size: float, camera_matrix: np.ndarray, distortion_coefficients: np.ndarray, color: tuple[int, int, int] = (0, 255, 0)) -> None:
     objPoints = np.array([
         [-tag_size / 2, -tag_size / 2, 0],
         [ tag_size / 2, -tag_size / 2, 0],
--- start_camera.py
+++ start_camera.py
@@ -15,15 +15,12 @@ camera_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
 
-camera_M, distort_coeff = get_camera_calibration(PATH_TO_CAMERA_CALIBRATION_FILE)
+camera_matrix, new_camera_matrix, distortion_coefficients = get_camera_calibration(PATH_TO_CAMERA_CALIBRATION_FILE)
 at_detector = apriltag_init()
 
 red_rgb = color_to_rgb("red")
 tag_size = 0.02 # in meters
-text_offset = 10
-arrow_scale = 0.1
 
-# template, template_keypoints, template_descriptors = create_object_fingerprint("test_a.png")
 
 while True:
     ret, frame = cam.read()
@@ -35,12 +32,11 @@ while True:
     np_gray_frame = img_to_np_gray(frame)
 
     res = detect_apriltag(at_detector, np_gray_frame)
-    # scene_with_detection, matches_visualization = find_object(frame, template, template_keypoints, template_descriptors)
 
     for tag in res:
-        draw_bbox(frame, tag, red_rgb)
-        draw_axis(frame, tag, arrow_scale, text_offset, red_rgb)
-        draw_depth(frame, tag, tag_size, camera_M, distort_coeff, red_rgb)
+        draw_apriltag_bbox(frame, tag, red_rgb)
+        draw_axis(frame, tag, red_rgb)
+        draw_depth(frame, tag, tag_size, camera_matrix, distorion_coefficients, red_rgb)
 
     cv2.imshow("camera", frame) 
 
